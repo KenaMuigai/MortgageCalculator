48e0982ee858005a6a8a4dd257bfee5a41e22825
kenamuigai <kenamuigai@gmail.com>
2022-03-02 12:12:22 +0300

    Initialised taking input of principle/initial amount and printing the title + principle and displaying it

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -1,8 +1,18 @@
 package com.java;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+//    I = P*R*T
+// Principle/Initial Amount(e.g 10000), Annual Interest Rate(e.g 3.92%), Period(Years), Mortgage displayed as a currency
     public static void main(String[] args) {
 	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        String title = "Mortgage Calculator";
+        System.out.println(title);
+        String principle = "Principle Amount: ";
+        System.out.print(principle);
+        Integer principleAmount = scanner.nextInt();
+        System.out.print(principleAmount);
     }
 }
